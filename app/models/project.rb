d03ff03df682fc36292bf87e763dd2037adcd00d
shubhankarsharma00 <shubhankarsharma78@gmail.com>
2019-03-29 11:29:15 +0530

    added uniqueness to tags

--- app/models/project.rb
+++ app/models/project.rb
@@ -73,7 +73,7 @@ class Project < ApplicationRecord
   end
 
   def tag_list=(names)
-    self.tags = names.split(",").map do |n|
+    self.tags = names.split(",").uniq.map do |n|
       Tag.where(name: n.strip).first_or_create!
     end
   end
